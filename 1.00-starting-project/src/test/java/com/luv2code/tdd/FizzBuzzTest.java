926fdef596e4bdc4964a563a3c313475791c7f6d
Bryan Personal <bryanlohjw97@gmail.com>
2023-08-06 18:23:26 +0800

    parameterized tests

--- 1.00-starting-project/src/test/java/com/luv2code/tdd/FizzBuzzTest.java
+++ 1.00-starting-project/src/test/java/com/luv2code/tdd/FizzBuzzTest.java
@@ -3,6 +3,8 @@ package com.luv2code.tdd;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -51,5 +53,32 @@ class FizzBuzzTest {
         String expected = "1";
 
         assertEquals(expected, FizzBuzz.compute(1), "Should return 1");
+    }
+
+    @DisplayName("Testing with Small data file")
+    @ParameterizedTest(name = "value={0}, expected={1}")
+    @CsvFileSource(resources = "/small-test-data.csv")
+    @Order(5)
+    void testSmallDataFile(int value, String expected) {
+
+        assertEquals(expected, FizzBuzz.compute(value));
+    }
+
+    @DisplayName("Testing with Medium data file")
+    @ParameterizedTest(name = "value={0}, expected={1}")
+    @CsvFileSource(resources = "/medium-test-data.csv")
+    @Order(6)
+    void testMediumDataFile(int value, String expected) {
+
+        assertEquals(expected, FizzBuzz.compute(value));
+    }
+
+    @DisplayName("Testing with Large data file")
+    @ParameterizedTest(name = "value={0}, expected={1}")
+    @CsvFileSource(resources = "/large-test-data.csv")
+    @Order(7)
+    void testLargeDataFile(int value, String expected) {
+
+        assertEquals(expected, FizzBuzz.compute(value));
     }
 }
